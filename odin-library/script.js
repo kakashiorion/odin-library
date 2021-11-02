a9773cdedd16143119212b9fc2a4ca9795e59c36
unknown <nitin.kool.1808@gmail.com>
2021-11-02 17:47:37 +0530

    Second

--- odin-library/script.js
+++ odin-library/script.js
@@ -76,16 +76,48 @@ function addBookToDisplay(book) {
   let bookDiv = document.createElement("div");
   let bookTitle = document.createElement("div");
   bookTitle.innerHTML = book.title;
+  bookTitle.style.fontSize = "3vh";
+  bookTitle.style.fontWeight = "600";
   let bookAuthor = document.createElement("div");
-  bookAuthor.innerHTML = book.author;
+  bookAuthor.innerHTML = "by " + book.author;
+  bookAuthor.style.fontSize = "2vh";
+  bookAuthor.style.fontWeight = "400";
   let bookPages = document.createElement("div");
-  bookPages.innerHTML = book.pages;
+  bookPages.innerHTML = "Pages: " + book.pages;
+  bookPages.style.fontSize = "1.5vh";
+
   let bookRead = document.createElement("div");
-  bookRead.innerHTML = book.read;
+  bookRead.innerHTML = "Read? ";
+
+  let bookReadCheck = document.createElement("input");
+  bookReadCheck.type = "checkbox";
+  if (book.read) {
+    bookReadCheck.checked = true;
+  }
+  bookReadCheck.addEventListener("click", () => {
+    changeBookReadStatus(book.index);
+  });
+  bookRead.appendChild(bookReadCheck);
+  bookRead.style.display = "flex";
+  bookRead.style.justifyContent = "center";
+
   bookDiv.appendChild(bookTitle);
   bookDiv.appendChild(bookAuthor);
   bookDiv.appendChild(bookPages);
   bookDiv.appendChild(bookRead);
+  bookDiv.style.alignSelf = "center";
+  bookDiv.style.justifySelf = "center";
+  bookDiv.style.margin = "10px";
+  bookDiv.style.padding = "10px 10px";
+  bookDiv.style.gap = "1vh";
+  bookDiv.style.backgroundColor = "white";
+  bookDiv.style.borderRadius = "10px";
+  bookDiv.style.height = "auto";
+  bookDiv.style.width = "90%";
+  bookDiv.style.display = "flex";
+  bookDiv.style.flexDirection = "column";
+  bookDiv.style.justifyContent = "space-evenly";
+  bookDiv.style.alignItems = "center";
   return bookDiv;
 }
 
@@ -113,7 +145,7 @@ displayBooks();
 
 //Testing here
 
-// let book1 = new Book("Dune", "Frank H", 200, false);
+// let book1 = new Book("Dune", "Frank H", 200, true);
 // console.log(book1);
 // addBookToLibrary(book1);
 // let library = fetchLibraryFromStorage();
